87c478217a4a360912ea976ed2109805e5828092
dianastanciu <dianastanciu13@yahoo.co.uk>
2019-08-27 15:02:26 +0300

    accesibility, hero section

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,21 @@
 let body = $('body');
 
-body.on('click', '.nav__open', function() {
-  $('.nav__list').addClass('active');
-  $('.header__logo').addClass('hidden');
-  $('.nav__open').addClass('hidden');
+body.on('keypress click', '.nav__open', function(e) {
+  if (e.which === 13 || e.type === 'click') {
+    e.preventDefault();
+    $('.nav__list').addClass('active');
+    $('.header__logo').addClass('hidden');
+    $('.nav__open').addClass('hidden');
+    return false;
+  }
 });
 
-body.on('click', '.nav__close', function() {
-  $('.nav__list').removeClass('active');
-  $('.header__logo').removeClass('hidden');
-  $('.nav__open').removeClass('hidden');
+body.on('keypress click', '.nav__close', function(e) {
+  if (e.which === 13 || e.type === 'click') {
+    e.preventDefault();
+    $('.nav__list').removeClass('active');
+    $('.header__logo').removeClass('hidden');
+    $('.nav__open').removeClass('hidden');
+    return false;
+  }
 });
